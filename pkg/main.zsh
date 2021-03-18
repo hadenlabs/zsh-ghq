f2e60c5fb7196053eac8a6a4be7fb3f676744f8c
@slovacus <slovacus@gmail.com>
2021-03-18 18:09:40 -0500

    style: format code zsh (#7)

--- pkg/main.zsh
+++ pkg/main.zsh
@@ -14,7 +14,6 @@ function ghq::pkg::main::factory {
         source "${ZSH_GHQ_PATH}"/pkg/linux.zsh
       ;;
     esac
-
     # shellcheck source=/dev/null
     source "${ZSH_GHQ_PATH}"/pkg/helper.zsh
 
